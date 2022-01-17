0f89c351816bcf57fe89c85da724cdfcb1a0cffb
Wiem Medimagh <91318202+WiemMedimagh@users.noreply.github.com>
2022-01-17 21:46:53 +0100

    Update NbrReducer.java

--- NbrReducer.java
+++ NbrReducer.java
@@ -11,7 +11,7 @@ public class NbrReducer extends Reducer<Text, NullWritable, Text, NullWritable> 
 
 int summ = 0;
 
-public void reduce(Text key,Iterable<NullWritable> values,Context context) throws IOException, InterruptedException{
+public void reduce(Text key,Iterable<IntWritable> values,Context context) throws IOException, InterruptedException{
     
 	int sum =0;
     for(IntWritable x : values){
